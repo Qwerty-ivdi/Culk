0cfe74ba224328dbaa16d09a5b141d33a6c21a83
Qwerty-ivdi <134587557+Qwerty-ivdi@users.noreply.github.com>
2024-05-15 11:07:50 +0300

    Update DecCulc.java
    dec

--- src/DecCulc.java
+++ src/DecCulc.java
@@ -1,7 +1,7 @@
 public class DecCulc implements Valid{
     @Override
     public boolean valid(String num) {
-        if(num.matches("^[-]?[1-9][0-9]+$"))
+        if(num.matches("^[-]?^[1-9][0-9]+$"))
             return true;
         else
             return false;
