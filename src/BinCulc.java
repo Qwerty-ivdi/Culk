8157a8a685b860b467061076cdb7fc2f880fea28
dimai <dimaivanov64101@gmail.com>
2024-05-15 09:05:40 +0300

    нельзя добовбавить 0 в начале

--- src/BinCulc.java
+++ src/BinCulc.java
@@ -2,7 +2,7 @@ public class BinCulc implements Valid {
 
     @Override
     public boolean valid(String num) {
-        if(num.matches("^[-]?[0-1]+$"))
+        if(num.matches("^[-]?[1][0-1]+$"))
             return true;
         else
             return false;
--- src/HexCulc.java
+++ src/HexCulc.java
@@ -1,7 +1,7 @@
 public class HexCulc implements Valid{
     @Override
     public boolean valid(String num) {
-        if(num.matches("[-]?[0-9A-Fa-f]+"))
+        if(num.matches("[-]?[1-9A-Fa-f][0-9A-Fa-f]+"))
             return true;
         else
             return false;
